a9e5052c3db3efa53f6019b9e34c7db1f71aca52
xrylsh <anibandarylrashell@gmail.com>
2026-09-08 01:31:10 +0800

    Refactor script.js for improved readability and structure

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ sendButton.addEventListener("click", function() {
 
     document.getElementById("thoughtCard").classList.remove("hidden");
 
-    fetch("/api/thoughts", {
+    fetch("https://thoughts-backend-3aof.onrender.com/api/thoughts", {
     method: "POST",
 
     headers: {
